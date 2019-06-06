e070069b61eb71c8601a355e10f32524134ad8a1
jasonwu0731 <jasonwu830731@gmail.com>
2019-06-06 19:33:25 +0800

    init

---
+++ myTest.py
@@ -0,0 +1,45 @@
+from utils.config import *
+from models.TRADE import *
+
+'''
+python3 myTest.py -ds= -path= -bsz=
+'''
+
+directory = args['path'].split("/")
+HDD = directory[2].split('HDD')[1].split('BSZ')[0]
+decoder = directory[1].split('-')[0] 
+BSZ = int(args['batch']) if args['batch'] else int(directory[2].split('BSZ')[1].split('DR')[0])
+args["decoder"] = decoder
+args["HDD"] = HDD
+print("HDD", HDD, "decoder", decoder, "BSZ", BSZ)
+
+if args['dataset']=='multiwoz':
+    from utils.utils_multiWOZ_DST import *
+else:
+    print("You need to provide the --dataset information")
+
+train, dev, test, test_special, lang, SLOTS_LIST, gating_dict, max_word = prepare_data_seq(False, args['task'], False, batch_size=BSZ)
+
+model = globals()[decoder](
+    int(HDD), 
+    lang=lang, 
+    path=args['path'], 
+    task=args["task"], 
+    lr=0, 
+    dropout=0,
+    slots=SLOTS_LIST,
+    gating_dict=gating_dict,
+    nb_train_vocab=max_word)
+
+if args["run_dev_testing"]:
+    print("Development Set ...")
+    acc_dev = model.evaluate(dev, 1e7, SLOTS_LIST[2]) 
+
+if args['except_domain']!="" and args["run_except_4d"]:
+    print("Test Set on 4 domains...")
+    acc_test_4d = model.evaluate(test_special, 1e7, SLOTS_LIST[2]) 
+
+print("Test Set ...")
+acc_test = model.evaluate(test, 1e7, SLOTS_LIST[3]) 
+
+
---
+++ myTrain.py
@@ -0,0 +1,64 @@
+from tqdm import tqdm
+import torch.nn as nn
+
+from utils.config import *
+from models.TRADE import *
+
+'''
+python myTrain.py -dec= -bsz= -hdd= -dr= -lr=
+'''
+
+early_stop = args['earlyStop']
+
+if args['dataset']=='multiwoz':
+    from utils.utils_multiWOZ_DST import *
+    early_stop = None
+else:
+    print("You need to provide the --dataset information")
+    exit(1)
+
+# Configure models and load data
+avg_best, cnt, acc = 0.0, 0, 0.0
+train, dev, test, test_special, lang, SLOTS_LIST, gating_dict, max_word = prepare_data_seq(True, args['task'], False, batch_size=int(args['batch']))
+
+model = globals()[args['decoder']](
+    hidden_size=int(args['hidden']), 
+    lang=lang, 
+    path=args['path'], 
+    task=args['task'], 
+    lr=float(args['learn']), 
+    dropout=float(args['drop']),
+    slots=SLOTS_LIST,
+    gating_dict=gating_dict, 
+    nb_train_vocab=max_word)
+
+# print("[Info] Slots include ", SLOTS_LIST)
+# print("[Info] Unpointable Slots include ", gating_dict)
+
+for epoch in range(200):
+    print("Epoch:{}".format(epoch))  
+    # Run the train function
+    pbar = tqdm(enumerate(train),total=len(train))
+    for i, data in pbar:
+        model.train_batch(data, int(args['clip']), SLOTS_LIST[1], reset=(i==0))
+        model.optimize(args['clip'])
+        pbar.set_description(model.print_loss())
+        # print(data)
+        # exit(1)
+
+    if((epoch+1) % int(args['evalp']) == 0):
+        
+        acc = model.evaluate(dev, avg_best, SLOTS_LIST[2], early_stop)
+        model.scheduler.step(acc)
+
+        if(acc >= avg_best):
+            avg_best = acc
+            cnt=0
+            best_model = model
+        else:
+            cnt+=1
+
+        if(cnt == args["patience"] or (acc==1.0 and early_stop==None)): 
+            print("Ran out of patient, early stop...")  
+            break 
+
